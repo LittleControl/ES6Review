5cc4c725d1ae45ac7de63973c2d911bd65631511
LittleControl <i@littlecontrol.cc>
2020-04-27 14:57:42 +0800

    02_变量的解构赋值

---
+++ 02_变量的解构赋值/test.js
@@ -0,0 +1,9 @@
+let t1 = undefined
+let t2 = 'undefined'
+let t3 = null
+console.log(t1 == t2) //false
+console.log(t1 === t2) //false
+console.log(t1 == t3) //true
+console.log(t1 === t3)//false
+console.log(t2 == t3)//false
+console.log(t2 === t3)//fasle
